7a8bdfd7c91b4343bba4fac99ab894c97ff72328
Joshuasanchez1 <nexusj31@outlook.com>
2024-08-20 18:00:12 -0500

    agregar cosas HTML

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
+<!DOCTYPE html>
+<html>
+<title>
+    Primer PHP
+</title>
+<body>
 
-<?php
+    <?php
 $nombre = "Joshua";
 $edad = 29;
 $altura = 1.77;
-$esEstudiante = true;
+$esEstudiante = false;
 
 echo "Nombre: $nombre<br>";
 echo "Edad: $edad<br>";
 echo "Altura: $altura<br>";
 echo "¿Es estudiante? " . ($esEstudiante ? "Sí" : "No");
 ?>
+</body>
+</html>
                         
