cff485d7c26c99f98701fe9e735e18c45fe91fdf
Joshuasanchez1 <nexusj31@outlook.com>
2024-08-20 17:57:17 -0500

    definidas variables de diferentes tipos

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<?php
+$nombre = "Joshua";
+$edad = 29;
+$altura = 1.77;
+$esEstudiante = true;
+
+echo "Nombre: $nombre<br>";
+echo "Edad: $edad<br>";
+echo "Altura: $altura<br>";
+echo "¿Es estudiante? " . ($esEstudiante ? "Sí" : "No");
+?>
+                        
