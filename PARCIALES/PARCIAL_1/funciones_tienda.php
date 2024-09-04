d91006431e8ecb523a8e98e2f0f91037968425d3
Joshuasanchez1 <nexusj31@outlook.com>
2024-09-03 19:57:43 -0500

    formato HTML problema 2 terminado

--- PARCIALES/PARCIAL_1/funciones_tienda.php
+++ PARCIALES/PARCIAL_1/funciones_tienda.php
@@ -1,42 +1,72 @@
-<?php
-function calcular_descuento($total_compra)
-{
-    if ($total_compra < 100) {
-        return 0;
-    } elseif ($total_compra >= 100 && $total_compra <= 500) {
-        return $total_compra * 0.05;
-    } elseif ($total_compra >= 501 && $total_compra <= 1000) {
-        return $total_compra * 0.10;
-    } elseif ($total_compra > 1000) {
-        return $total_compra * 0.15;
-    }
-}
+<!DOCTYPE html>
+<html>
 
-function aplicar_impuesto($subtotal)
-{
-    return $subtotal * 0.07;
-}
+<head>
+    <title>
 
-function calcular_total($subtotal, $descuento, $impuesto)
-{
-    return $subtotal - $descuento + $impuesto;
-}
-$precios = ['camisa' => 50, 'pantalon' => 70, 'zapatos' => 80, 'calcetines' => 10, 'gorra' => 25];
-$carrito = [
-    'camisa' => 2,
-    'pantalon' => 1,
-    'zapatos' => 1,
-    'calcetines' => 3,
-    'gorra' => 0
-];
-foreach ($carrito as $key => $value) {
-    $compra = $value * $precios[$key];
-    $descuento = calcular_descuento($compra);
-    $impuesto = aplicar_impuesto($compra);
-    $total = calcular_total($compra, $descuento, $impuesto);
-    echo "Subtotal: $" . $compra . "<br>";
-    printf("Descuento aplicado: $%.2f<br>", $descuento);
-    printf("Impuesto: $%.2f<br>", $impuesto);
-    printf("Total a pagar: $%.2f<br>", $total);
-    echo "<br>";
-}
+    </title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>Articulo comprado</th>
+            <th>Precio</th>
+            <th>Cantidad comprada</th>
+            <th>Sub Total</th>
+            <th>Descuento</th>
+            <th>Impuesto</th>
+            <th>Total</th>
+        </tr>
+
+        <?php
+        function calcular_descuento($total_compra)
+        {
+            if ($total_compra < 100) {
+                return 0;
+            } elseif ($total_compra >= 100 && $total_compra <= 500) {
+                return $total_compra * 0.05;
+            } elseif ($total_compra >= 501 && $total_compra <= 1000) {
+                return $total_compra * 0.10;
+            } elseif ($total_compra > 1000) {
+                return $total_compra * 0.15;
+            }
+        }
+
+        function aplicar_impuesto($subtotal)
+        {
+            return $subtotal * 0.07;
+        }
+
+        function calcular_total($subtotal, $descuento, $impuesto)
+        {
+            return $subtotal - $descuento + $impuesto;
+        }
+        $precios = ['camisa' => 50, 'pantalon' => 70, 'zapatos' => 80, 'calcetines' => 10, 'gorra' => 25];
+        $carrito = [
+            'camisa' => 2,
+            'pantalon' => 1,
+            'zapatos' => 1,
+            'calcetines' => 3,
+            'gorra' => 0
+        ];
+        foreach ($carrito as $key => $value) {
+            $compra = $value * $precios[$key];
+            $descuento = calcular_descuento($compra);
+            $impuesto = aplicar_impuesto($compra);
+            $total = calcular_total($compra, $descuento, $impuesto);
+            echo "<tr>";
+            echo "<td>" . $key . "</td>";
+            echo "<td>" . $precios[$key] . "</td>";
+            echo "<td>" . $carrito[$key] . "</td>";
+            echo "<td>" . $compra . "</td>";
+            echo "<td>" . $descuento . "</td>";
+            echo "<td>" . $impuesto . "</td>";
+            echo "<td>" . $total . "</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+
+</html>
